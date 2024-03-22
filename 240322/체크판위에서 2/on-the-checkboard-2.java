1ab3f74793d50c99d2514bf9101d43f0dc1b837d
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-22 18:32:37 +0900

    Title: 체크판위에서 2, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240322/체크판위에서 2/on-the-checkboard-2.java
+++ 240322/체크판위에서 2/on-the-checkboard-2.java
@@ -1,59 +1,50 @@
 import java.io.*;
 import java.util.*;
 
-
 public class Main {
-
-    private static int result = 0;
+    private static int R, C;
     private static char[][] grid;
+    private static int result = 0;
+    private static char[] pos = new char[] {'W', 'B', 'W', 'B'};
 
     public static void main(String[] args) throws IOException {
+        // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] size = new int[2];
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");;
-        for(int i = 0; i < 2; ++i) {
-            size[i] = Integer.parseInt(st.nextToken());
-        }
-
-        final int R = size[0];
-        final int C = size[1];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
         grid = new char[R][C];
-        for(int i = 0; i < R; ++i) {
+        for (int i = 0; i < R; ++i) {
             st = new StringTokenizer(br.readLine(), " ");
             for (int j = 0; j < C; ++j) {
                 grid[i][j] = st.nextToken().charAt(0);
             }
         }
-        
-        trace(0, 0, 1);
+
+        trace(0, 0, 0);
 
         System.out.println(result);
-        
+
+
     }
 
-    private static void trace(int x, int y, int step) {
-        if (y >= grid.length || x >= grid[y].length) return;
+    private static void trace(int y, int x, int step) {
 
-        if (step == 2 && grid[y][x] != 'B') return;
-        if (step == 3 && grid[y][x] != 'W') return;
-        if (step == 4 && grid[y][x] != 'B') return;
-            
-        if (y == grid.length - 1 && x == grid[y].length - 1 && grid[y][x] == 'B' && step==4) {
+        if (y >= R || x >= C) return;
+
+        if (pos[step] != grid[y][x]) return;
+        
+        if (y == R-1 && x == C-1 && step == 3) {
             result++;
             return;
         }
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[i].length; ++j) {
-                int nextY = y + i + 1;
-                int nextX = x + j + 1;
 
-                if (nextY - y < 1 || nextX - x < 1) continue;
-                trace(nextX, nextY, step+1);
+        for (int i = 1; i < R; ++i) {
+            for (int j = 1; j < C; ++j) {
+                trace(y+i, x+j, step+1);
             }
         }
-    }   
-    
+    }
 }
