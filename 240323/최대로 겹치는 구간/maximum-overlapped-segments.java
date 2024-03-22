9e0fffcb5bdca17c2f37a9a89321daa96758cc62
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-23 01:17:42 +0900

    Title: 최대로 겹치는 구간, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

--- 240323/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240323/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -2,50 +2,36 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    
     public static void main(String[] args) throws IOException {
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
 
-        int[] negativeIntervals = new int[101];
-        int[] positiveIntervals = new int[101];
+        int[] intervals = new int[201];
+        final int offset = 100;
+        
         StringTokenizer st;
         for (int interval = 0; interval < N; ++interval) {
             st = new StringTokenizer(br.readLine(), " ");
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
+            int start = Integer.parseInt(st.nextToken()) + offset;
+            int end = Integer.parseInt(st.nextToken()) + offset;
 
-            if (start < 0 && end < 0) {
-                for (int i = Math.abs(end); i < Math.abs(start); ++i) {
-                    if (i == start || i == end) continue;
-                    negativeIntervals[i] += 1;
-                }
-            } else if (start < 0 && end >= 0) {
-                for (int i = Math.abs(start); i < negativeIntervals.length; ++i) {
-                    if (i == start || i == end) continue;
-                    negativeIntervals[i] += 1;
-                }
-                for (int i = 0; i <= end; ++i) {
-                    if (i == start || i == end) continue;
-                    positiveIntervals[i] += 1;
-                }
-
-            } else {
-                for (int i = start; i <=end; ++i) {
-                    if (i == start || i == end) continue;
-                    positiveIntervals[i] += 1;
-                }
+            for (int i = start; i < end; ++i) {
+                intervals[i] += 1;
             }
+            
         }
 
         int result = Integer.MIN_VALUE;
-        for (int i = 0; i < negativeIntervals.length; ++i) {
-            if (result < negativeIntervals[i]) result = negativeIntervals[i];
-        }
-        for (int i = 0; i < positiveIntervals.length; ++i) {
-            if (result < positiveIntervals[i]) result = positiveIntervals[i];
+        for (int i = 0; i < intervals.length; ++i) {
+            result = Math.max(result, intervals[i]);
         }
 
         System.out.println(result);
+
+        
+
+        
     }
 }
