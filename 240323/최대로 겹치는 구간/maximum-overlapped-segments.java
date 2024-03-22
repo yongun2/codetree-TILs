669f5c4bc480880ef862171a0552c3735ad750e3
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-23 00:30:43 +0900

    Title: 최대로 겹치는 구간, Time: 105ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240323/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240323/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -7,22 +7,43 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         final int N = Integer.parseInt(br.readLine());
 
-        int[] intervals = new int[101];
+        int[] negativeIntervals = new int[101];
+        int[] positiveIntervals = new int[101];
         StringTokenizer st;
-        for (int i = 0; i < N; ++i) {
+        for (int interval = 0; interval < N; ++interval) {
             st = new StringTokenizer(br.readLine(), " ");
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
 
-            for (int j = start; j <=end; ++j) {
-                if (j == start || j == end) continue;
-                intervals[j] += 1;
+            if (start < 0 && end < 0) {
+                for (int i = Math.abs(end); i < Math.abs(start); ++i) {
+                    if (i == start || i == end) continue;
+                    negativeIntervals[i] += 1;
+                }
+            } else if (start < 0 && end >= 0) {
+                for (int i = Math.abs(start); i < negativeIntervals.length; ++i) {
+                    if (i == start || i == end) continue;
+                    negativeIntervals[i] += 1;
+                }
+                for (int i = 0; i <= end; ++i) {
+                    if (i == start || i == end) continue;
+                    positiveIntervals[i] += 1;
+                }
+
+            } else {
+                for (int i = start; i <=end; ++i) {
+                    if (i == start || i == end) continue;
+                    positiveIntervals[i] += 1;
+                }
             }
         }
 
         int result = Integer.MIN_VALUE;
-        for (int i = 0; i < intervals.length; ++i) {
-            if (result < intervals[i]) result = intervals[i];
+        for (int i = 0; i < negativeIntervals.length; ++i) {
+            if (result < negativeIntervals[i]) result = negativeIntervals[i];
+        }
+        for (int i = 0; i < positiveIntervals.length; ++i) {
+            if (result < positiveIntervals[i]) result = positiveIntervals[i];
         }
 
         System.out.println(result);
