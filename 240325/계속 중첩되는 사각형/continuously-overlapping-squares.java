b834c659cb2a48cfa4ba8dac3561ecd0486b2fc3
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-25 16:50:23 +0900

    Title: 계속 중첩되는 사각형, Time: 100ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 240325/계속 중첩되는 사각형/continuously-overlapping-squares.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        final int N = Integer.parseInt(br.readLine());
+        final int offset = 100;
+        
+        int[][] area = new int[201][201];
+        StringTokenizer st;
+        for (int i = 0; i < N; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int x1 = Integer.parseInt(st.nextToken()) + offset;
+            int y1 = Integer.parseInt(st.nextToken()) + offset;
+            int x2 = Integer.parseInt(st.nextToken()) + offset;
+            int y2 = Integer.parseInt(st.nextToken()) + offset;
+
+            for (int j = y1; j < y2; j++) {
+                    for (int k = x1; k < x2; ++k) {
+                        if (i % 2 != 0) area[j][k] = 1;
+                        else area[j][k] = 0;
+                    }
+            }
+        }
+
+        int result = 0;
+        for (int i = 0; i < 201; ++i) {
+            for (int j = 0; j < 201; ++j) {
+                if (area[i][j] == 1) result++;
+            }
+        }
+        System.out.println(result);
+    }
+}
