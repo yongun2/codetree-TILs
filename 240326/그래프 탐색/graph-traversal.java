173093705ce2526462afdc81979337de9a18fbc2
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-26 18:45:27 +0900

    Title: 그래프 탐색, Time: 175ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240326/그래프 탐색/graph-traversal.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static Map<Integer, List<Integer>> graph = new HashMap<>();
+    private static Set<Integer> visited = new HashSet<>();
+    private static int N, M;
+    private static int result = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < M; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            if (!graph.containsKey(x)) {
+                graph.put(x, new ArrayList<Integer>());
+            }
+            graph.get(x).add(y);
+        }
+        
+        DFS(1);
+
+        System.out.println(result);
+    }
+
+    private static void DFS(int vertex) {
+        
+        if (visited.contains(vertex)) return;
+
+        if (vertex != 1) result ++;
+
+        visited.add(vertex);
+        List<Integer> neighbors = graph.get(vertex);
+        if (neighbors == null ) return;
+        for (int next: neighbors) {
+            DFS(next);
+        }
+    }
+}
