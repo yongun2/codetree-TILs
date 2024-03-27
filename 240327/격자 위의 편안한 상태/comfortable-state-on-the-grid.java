fd3c9534419a47e352ace251587a41a776ffaa91
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-27 15:13:32 +0900

    Title: 격자 위의 편안한 상태, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240327/격자 위의 편안한 상태/comfortable-state-on-the-grid.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static int N,M;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[N][N];
+
+        int[] dx = new int[] {0, 1, 0, -1};
+        int[] dy = new int[] {-1, 0, 1, 0};
+
+        for (int i = 0; i < M; ++i) {
+            st = new StringTokenizer(br.readLine(), " ");
+
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            grid[y][x] = 1;
+            
+            int count = 0;
+            for (int j = 0; j < 4; ++j) {
+                int nx = x + dx[j];
+                int ny = y + dy[j];
+
+                if (inRange(nx, ny) && grid[ny][nx] == 1) {
+                    count += 1;
+                }
+            }
+            
+            if (count == 3) System.out.println(1);
+            else System.out.println(0);
+        }
+    }
+
+    private static boolean inRange(int x, int y) {
+        return x >= 0 && x < M && y >= 0 && y < N;
+    }
+}
