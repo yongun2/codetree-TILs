b3f64c8b04077d47688f82d2fe952e1fcb671d93
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-28 01:00:16 +0900

    Title: 마을 구분하기, Time: 108ms, Memory: 9MB, Status: Passed - CodeTree

--- 240328/마을 구분하기/seperate-village.java
+++ 240328/마을 구분하기/seperate-village.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static int villageCount = 0;
     public static int memberCount = 0;
-    public static List<Integer> members = new ArrayList<>(); 
+    public static List<Integer> members = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -28,41 +28,48 @@ public class Main {
 
         for (int i = 0; i < N; ++i) {
             for (int j = 0; j < N; ++j) {
-                if (DFS(i, j)) {
-                    villageCount += 1;
-                    members.add(memberCount);
-                }
-                memberCount = 0;
+                if (canGo(i, j)) {
+                    visited[i][j] = true;
+                    memberCount = 1;
+                    
+                    DFS(i, j);
+                    members.add(memberCount);   
+                }    
             }
+            
         }
-        System.out.println(villageCount);
-        members.stream().sorted().forEach(System.out::println);
+
+        System.out.println(members.size());
+        Collections.sort(members);
+        for (Integer count: members) {
+            System.out.println(count);
+        }
     }
 
-    public static boolean DFS(int x, int y) {
-        
-        if (!inRange(x, y)) return false;
-
-        if (isBlocked(x, y) || visited[y][x]) return false;
+    public static void DFS(int y, int x) {
+        int[] dx = new int[] {0, 1, 0, - 1};
+        int[] dy = new int[] {-1, 0, 1, 0};
 
         visited[y][x] = true;
-        memberCount += 1;
-
-        int[] dx = new int[] {0, 1, 0, -1};
-        int[] dy = new int[] {-1, 0, 1, 0};
         for (int i = 0; i < 4; ++i) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
-            DFS(nx, ny);
+            int nx = dx[i] + x;
+            int ny = dy[i] + y;
+            if (canGo(ny, nx)) {
+                memberCount += 1;
+                DFS(ny, nx);
+            }
         }
+    }
+
+    public static boolean canGo(int y, int x) {
+        if (!inRange(y, x)) return false;
+
+        if (grid[y][x] == 0 || visited[y][x]) return false;
+
         return true;
     }
 
-    public static boolean inRange(int x, int y) {
-        return x >= 0 && x < N && y >= 0 && y < N;
-    }
-
-    public static boolean isBlocked(int x, int y) {
-        return grid[y][x] == 0;
+    public static boolean inRange(int y, int x) {
+        return y >= 0 && y < N && x >= 0 && x < N;
     }
 }
