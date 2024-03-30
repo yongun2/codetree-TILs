c0d3c43e94f43b7a23a6ecf876c8d027520e70e6
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-03-30 17:01:10 +0900

    Title: 두 수의 합, Time: 272ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 240330/두 수의 합/sum-of-two-num.java
+++ 240330/두 수의 합/sum-of-two-num.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class Main {
     public static int N, K;
+    public static int[] array;
     public static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
@@ -12,16 +13,34 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
+        array = new int[N];
         st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < N; ++i) {
             int val = Integer.parseInt(st.nextToken());
-            map.put(val, Math.abs(K - val));
-        }
-        int result = 0;
-        for (Integer key: map.keySet()) {
-            if (map.containsKey(key)) result += 1;
+            array[i] = val;
+            map.put(val, map.getOrDefault(val, 0) + 1);
         }
 
-        System.out.println(result / 2);
+    
+        int result = 0;
+        for (int num: array) {
+            int need = K - num;
+            if (!map.containsKey(need)) continue;
+            
+            if (map.get(num) == 0 && map.get(need) == 0) continue;
+            if (num == need) {
+                int count = map.get(need);
+                result += (count * (count -1)) / 2;
+                map.put(num, 0);
+            } else {
+                result += 1;
+                map.put(num, map.get(num) - 1);
+                map.put(need, map.get(need) - 1);
+            }
+        }
+    
+        System.out.println(result);
     }
+
+    
 }
