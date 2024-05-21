69e4059ae6bc455565e14894bd16ffb178e5b2fd
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-05-21 09:35:20 +0900

    Title: 수열 합치기, Time: 1370ms, Memory: 43MB, Status: Passed - CodeTree

---
+++ 240521/수열 합치기/concatenation-of-sequences.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static int N, M;
+    private static int[] array;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        array = new int[N + M];
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < N; ++i) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = N; i < array.length; ++i) {
+            array[i] = Integer.parseInt(st.nextToken());
+        }
+
+        array = Arrays.stream(array)
+                        .sorted()
+                        .toArray();
+
+        for (int i = 0; i < array.length; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        
+    }
+}
