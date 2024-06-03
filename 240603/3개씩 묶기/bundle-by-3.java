5144a188d041bfe4c38bdc64e48f2324ed414e1d
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-06-03 16:33:18 +0900

    Title: 3개씩 묶기, Time: 273ms, Memory: 18MB, Status: Passed - CodeTree

--- 240603/3개씩 묶기/bundle-by-3.java
+++ 240603/3개씩 묶기/bundle-by-3.java
@@ -1,10 +1,12 @@
 import java.io.*;
 import java.util.*;
+import java.util.stream.*;
 
 public class Main {
 
     private static int N;
     private static int[] array;
+    private static List<Integer> arrayList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,6 +20,7 @@ public class Main {
         }
 
         Arrays.sort(array);
+                                
 
         if (array.length / 3 == 0) {
             int sum = 0;
@@ -31,17 +34,30 @@ public class Main {
 
         int delimeter = array.length / 3;
         int sum = 0;
-        for (int i = 1; i <= delimeter; ++i) {
-            for (int j = (i - 1) * 3 + 1; j < (i - 1) * 3 + 3; ++j) {
-                sum += array[j];
-            }
-        }
 
-        for (int i = delimeter * 3; i < array.length; ++i) {
+        for (int i = 1; i <= delimeter; ++i) {
+            for (int j = array.length - (i - 1) * 3 - 1; j > array.length - (i - 1) * 3 - 3; --j) {
+                sum += array[j];
+                // System.out.print(array[j] + " ");
+            }
+            // System.out.println();
+        }
+        // 0 1 2
+        // 3 4 5
+
+        // 0 ~ 8
+        //
+
+        for (int i = array.length - delimeter * 3 - 1 ; i >= 0; --i) {
             sum += array[i];
+            // System.out.print(array[i] + " ");
+
         }
 
         System.out.println(sum);
+
+        // 17467410
+        // 17467738
         
         
     }
