47d830adc86e0c4e30812c4357e2c25423a3d954
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-06-03 16:47:42 +0900

    Title: 원 이루기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

--- 240603/원 이루기/circle.java
+++ 240603/원 이루기/circle.java
@@ -7,17 +7,21 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         
         N = Integer.parseInt(br.readLine());
 
         for (int i = 1; i <= N; ++i) {
             if (i == N && N % 2 != 0) {
-                System.out.print("3 ");
+                bw.write("3 ");
+                // System.out.print("3 ");
                 break;
             }
-            System.out.print(i % 2 == 0 ? "2 " : "1 ");
+            bw.write(i % 2 == 0 ? "2 " : "1 ");
+            // System.out.print(i % 2 == 0 ? "2 " : "1 ");
         }
+        bw.flush();
+        bw.close();
         
 
     }
