331ff73d9285c1a075db68cd0cc3349a904c3e0c
yongun2 <76439014+yongun2@users.noreply.github.com>
2024-06-03 17:01:32 +0900

    Title: 수의 곱셈, Time: 107ms, Memory: 8MB, Status: Passed - CodeTree

--- 240603/수의 곱셈/multiplication-of-numbers.java
+++ 240603/수의 곱셈/multiplication-of-numbers.java
@@ -11,6 +11,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < 3; ++i) {
@@ -22,7 +23,10 @@ public class Main {
             combine(0, i);    
         }
         
-        System.out.println(result);
+        bw.write(String.valueOf(result));
+        bw.flush();
+        bw.close();
+        // System.out.println(result);
 
     }
 
